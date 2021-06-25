5a5c7ccd1f2279d38b000635edd6da540381324b
jinnyfruit <jinnyfruit0527@gmail.com>
2021-06-25 21:52:55 +0900

    first code

--- main.c
+++ main.c
@@ -11,10 +11,114 @@ void gomoku(); // Tuna
 void tic_tac_toe(); //jj0526
 //ljlee
 
+int count = 1;
+int secreteNum;
+int checkNum;
+int Success_Key = 0;
+void check(int, int);
+
 //function definitions
 void connect_four(); //asas6978
 
-void num_baseball(); //jinnyfruit
+void num_baseball(){
+     printf("★Welcome to number baseball game★\n\n");
+    printf("-> You can predict a three-digit number a total of 9 times.\n");
+    printf("-> if the number and digits match, S.\n");
+    printf("-> if only the number matches and the number of digits is different, B.\n");
+    printf("-> if none of the numbers match, OUT.\n");
+
+    srand(time(NULL));
+    secreteNum = (rand() % 999) + 100;    //Generate three-digit natural number to match
+    printf("secrete Num: %d\n", secreteNum);
+
+    while (count < 10) {
+
+        printf("\nType a three-digit number (%d time):", count);
+        scanf("%d", &checkNum);
+
+        if (checkNum == secreteNum) {
+            printf("\nSuccess!\n");
+            Success_Key++;
+            break;
+        }
+
+        else {
+            check(secreteNum, checkNum);
+        }
+
+        count++;
+    }
+
+    if (Success_Key == 0) {
+        printf("\nFAIL!\n");
+    }
+}
+
+void check(int secreteNum, int checkNum){
+ int temp;
+    int i, j;
+
+    int Strike = 0;
+    int Ball = 0;
+    int Out = 0;
+    int compare_list[2][3];
+
+    compare_list[0][0] = secreteNum / 100;
+    secreteNum = secreteNum % 100;
+    compare_list[0][1] = secreteNum / 10;
+    compare_list[0][2] = secreteNum % 10;
+
+    compare_list[1][0] = checkNum / 100;
+    checkNum = checkNum % 100;
+    compare_list[1][1] = checkNum / 10;
+    compare_list[1][2] = checkNum % 10;
+
+
+
+    for (i = 0; i < 3; i++) {
+
+        temp = 0;
+
+        for (j = 0; j < 3; j++) {
+
+            if (compare_list[0][i] == compare_list[1][j]) {
+
+                if (i == j) {
+
+                    Strike++;
+
+                    if (temp > 0) {
+                        Ball = Ball - temp;
+                    }
+
+                    break;
+                }
+                else {
+
+                    Ball++;
+
+                    if (temp > 0) {
+                        Ball = Ball - temp;
+                    }
+                    temp++;
+                }
+            }
+            else
+                Out++;
+        }
+    }
+
+    printf("\n[result]\n");
+
+    if (Strike == 0 && Ball == 0 && Out > 0)
+        printf("OUT!\n");
+    else {
+        if (Strike > 0)
+            printf("S:%d \n", Strike);
+        if (Ball > 0)
+            printf("B:%d \n", Ball);
+    }
+}
 
 void gomoku(); // Tuna
 
