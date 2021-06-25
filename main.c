20ae25f51289361d160e61509eb2ae9ad82877cd
LJLee37 <ljlee3759@gmail.com>
2021-06-25 15:30:08 +0900

    Add main.c

---
+++ main.c
@@ -0,0 +1,22 @@
+//headers
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+//function prototypes
+void connect_four(); //asas6978
+void num_baseball(); //jinnyfruit
+void gomoku(); // Tuna
+void tic_tac_toe(); //jj0526
+//ljlee
+
+//function definitions
+void connect_four(); //asas6978
+
+void num_baseball(); //jinnyfruit
+
+void gomoku(); // Tuna
+
+void tic_tac_toe(); //jj0526
+
