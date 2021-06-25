81c4d644392335c44f3a224f173be4a932c1fef7
LJLee37 <ljlee3759@gmail.com>
2021-06-25 15:55:01 +0900

    main() complete

--- main.c
+++ main.c
@@ -23,5 +23,49 @@ void tic_tac_toe(); //jj0526
 //main
 int main()
 {
-    //
+    const char game_names[4][20] = {"connect four", "number baseball", "gomoku", "tic-tac-toe"};
+    const int game_count = 4;
+    char sys = '2';
+    do
+    {
+        printf("Choose if you are using Windows or Unix! Unix includes macOS and Linux.\n");
+        printf("1. Windows, 2. Unix. your system : ");
+        scanf(" %c", &sys);
+    } while(sys != '1' && sys != '2');
+    while(1)
+    {
+        printf("Welcome to Minigame Heaven!\n");
+        printf("We have currently %d games.\n", game_count);
+        for(int i = 0; i < game_count; ++i)
+            printf("%d. %s\n", i + 1, game_names[i]);
+        printf("Type the number of each game or 0 if you want to exit.\n");
+        printf("your choice : ");
+        char choice = '0';
+        scanf(" %c", &choice);
+        if (sys == '1')
+            system("cls");
+        else
+            system("clear");
+        switch(choice)
+        {
+        case '0':
+            printf("Thanks for playing! Hope to see you soon!\n");
+            return 0;
+        case '1':
+            connect_four();
+            break;
+        case'2':
+            num_baseball();
+            break;
+        case '3':
+            gomoku();
+            break;
+        case '4':
+            tic_tac_toe();
+            break;
+        default:
+            printf("You typed a wrong number! Please try again!\n");
+            break;
+        }
+    }
 }
